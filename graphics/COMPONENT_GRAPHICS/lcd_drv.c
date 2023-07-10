0c2defd58d38c53cb065c58f692dca9df0b171e9
Peeyush Kumar <peeyush.ei@gmail.com>
2023-07-10 12:51:25 +0530

    Update lcd_drv.c

--- graphics/COMPONENT_GRAPHICS/lcd_drv.c
+++ graphics/COMPONENT_GRAPHICS/lcd_drv.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-int main()
+int lcd_init()
 {
     int a = 7;
-    int b =  6;
+    int b =  5;
     int c = 0;
     c = a + b;
-    return 0;
+    return c;
+}
+
+int lcd_print_line()
+{
+    int a = 7;
+    int b =  5;
+    int c = 0;
+    c = a + b;
+    return c;
 }
